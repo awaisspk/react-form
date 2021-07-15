f536f1f65be12e8654cfc0ff08fa8e7865954c9a
awaisspk <iamawaisiqball@gmail.com>
2021-07-15 12:59:10 +0500

    sign up form logic complete

---
+++ src/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormSignup from './FormSignup.jsx';
+
+const Form = () => {
+  return (
+    <div>
+      <FormSignup />
+    </div>
+  );
+};
+
+export default Form;
