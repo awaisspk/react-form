0ad28ece31c5afe4a6908c05223561bca826fbf9
awaisspk <iamawaisiqball@gmail.com>
2021-07-15 16:26:00 +0500

    form 2 formik basics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-// import logo from './logo.svg';
 import './App.css';
+import Basic from './components/Form.jsx';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Basic />
+    </div>
+  );
 }
 
 export default App;
